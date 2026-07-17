51efdd6697c77ee630b24346b634042b95e5163a
Wonhee Seo <sallybffsarah@gmail.com>
2026-07-17 10:35:06 +0900

    랜덤함수 활용

---
+++ chapter01_basic/03-1_random.py
@@ -0,0 +1,9 @@
+from random import * 
+date = randrange(3, 29)
+print("오프라인 스터디 모임 날짜는 매월 " + str(date) + "일로 선정되었습니다")
+print("오프라인 스터디 모임 날짜는 매월 " + str(date) + "일로 선정되었습니다")
+print("오프라인 스터디 모임 날짜는 매월 " + str(date) + "일로 선정되었습니다")
+
+from random import * 
+date = randint(4, 28)
+print("오프라인 스터디 모임 날짜는 매월 " + str(date) + "일로 선정되었습니다")
