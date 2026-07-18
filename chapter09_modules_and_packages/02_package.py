2ebb081e1c069588b068834351793c4590c913bb
Wonhee Seo <sallybffsarah@gmail.com>
2026-07-19 08:49:53 +0900

    Chapter09- Package

--- chapter09_modules_and_packages/02_package.py
+++ chapter09_modules_and_packages/02_package.py
@@ -1,3 +1,15 @@
+# ===========================================
+# 패키지 import
+# ===========================================
+
 import travel.thailand
 trip_to = travel.thailand.ThailandPackage()
+trip_to.detail()
+
+# ===========================================
+# 클래스 직접 import
+# ===========================================
+
+from travel.thailand import ThailandPackage
+trip_to = ThailandPackage()
 trip_to.detail()
