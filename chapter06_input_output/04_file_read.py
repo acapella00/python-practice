9e7602648261869c5d46f4c5a95b23f52ba81da5
Wonhee Seo <sallybffsarah@gmail.com>
2026-07-18 13:33:52 +0900

    Chapter06- File read

---
+++ chapter06_input_output/04_file_read.py
@@ -0,0 +1,33 @@
+score_file = open("score.txt", "r", encoding="utf8")
+print(score_file.read())
+score_file.close()
+
+score_file = open("score.txt", "r", encoding="utf8")
+print(score_file.readline(), end="") # 줄별로 읽기, 한 줄 일고 커서는 다음줄로 이동
+print(score_file.readline(), end="")
+print(score_file.readline(), end="")
+print(score_file.readline(), end="")
+score_file.close()
+
+score_file = open("score.txt", "r", encoding="utf8") # 파일이 몇줄인지 모름
+while True:
+    line = score_file.readline()
+    if not line:
+        break
+    print(line)
+score_file.close()
+
+score_file = open("score.txt", "r", encoding="utf8") # 줄바꿈 없이
+while True:
+    line = score_file.readline()
+    if not line:
+        break
+    print(line, end="")
+score_file.close()
+
+score_file = open("score.txt", "r", encoding="utf8")
+lines = score_file.readlines() # list 형태로 저장
+for line in lines:
+    print(line, end="")
+
+score_file.close()
