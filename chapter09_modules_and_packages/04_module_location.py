0142405b631d28ef4cd6e91150f4697ad26e66c3
Wonhee Seo <sallybffsarah@gmail.com>
2026-07-19 10:04:23 +0900

    Chapter09- Module Location

---
+++ chapter09_modules_and_packages/04_module_location.py
@@ -0,0 +1,8 @@
+from travel import * 
+
+import inspect
+import random
+
+print(inspect.getfile(random))
+print(inspect.getfile(thailand))
+print(inspect.getfile(vietnam))
--- chapter09_modules_and_packages/travel/__init__.py
+++ chapter09_modules_and_packages/travel/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ["vietnam"]
+__all__ = ["thailand", "vietnam"]
